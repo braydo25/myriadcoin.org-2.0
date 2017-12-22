fd512b42d3bd983eb8a2c5393955b6eb199757e8
Braydon Batungbacal <me@braydonb.com>
2017-12-21 16:33:15 -0800

    Fat first commit, still working on initial design of site.

---
+++ src/javascript/Main.js
@@ -0,0 +1,27 @@
+$(function() {
+  $(window).scroll(function() {
+    const scrollTop = $(document).scrollTop();
+    const heroHeight = $('#hero').height();
+    const headerHeight = $('#header').outerHeight();
+    const $header = $('#header');
+
+    if (scrollTop >= heroHeight - headerHeight && !$header.hasClass('pinned')) {
+      $header.addClass('pinned').css('top', -1 * headerHeight + 'px');
+      $header.animate({
+        top: 0
+      }, 250);
+    }
+
+    if (scrollTop < heroHeight - headerHeight && $header.hasClass('pinned') && $header.css('top') == '0px') {
+      $header.animate({
+        top: -1 * headerHeight + 'px'
+      }, 250, function() {
+        $(this).removeClass('pinned').css('top', 0);
+      });
+    }
+
+    if (scrollTop < headerHeight + 50 && $header.is(':animated')) {
+      $header.finish();
+    }
+  });
+});
